d942b102eb4cba61ea3578834fd2216b346188a4
Mathias Roux <mathiasroux.pro@gmail.com>
2022-05-15 13:10:48 +0200

    borders wip

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+import '../styles/App.css'
+
+export function App() {
+	return (
+		<div>
+			<div className="border border-top"></div>
+			<div className="border border-right"></div>
+			<div className="border border-bottom"></div>
+			<div className="border border-left"></div>
+		</div>
+	)
+}
